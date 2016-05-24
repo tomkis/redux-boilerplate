35f783d3696b05f51ce56209ac1ccc3a470e64e2
Tomas Weiss <tomasw@salsitasoft.com>
2016-05-24 14:07:05 +0200

    getting rid of unnecessary deps

--- webpack/webpack.frontend.config.babel.js
+++ webpack/webpack.frontend.config.babel.js
@@ -1,19 +1,6 @@
-import path from 'path';
 import webpack from 'webpack';
 
 import config from './webpack.prod.frontend.config.babel';
-import pathConfig from './webpack.path.config.babel';
-
-// adding hot reload and source maps
-config.module.loaders = [{
-  test: /\.js$/,
-  loaders: ['react-hot', 'babel'],
-  include: path.join(__dirname, '../src')
-}, {
-  test: /\.styl$/,
-  // the stylus-loader resolves paths in reversed order - so reverse root
-  loader: `style!css?sourceMap!postcss!stylus?paths[]=${pathConfig.root.slice().reverse().join(',paths[]=')}`
-}];
 
 // exclude uglify
 config.plugins = [
