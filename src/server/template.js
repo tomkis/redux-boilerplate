5ec3b3b8395f66d336a889fa2290f6f2f1fac8f2
Tomas Weiss <tomasw@salsitasoft.com>
2016-06-07 13:53:29 +0200

    incorrect favicon URL

--- src/server/template.js
+++ src/server/template.js
@@ -3,8 +3,8 @@ export default (content, state, useCss = false) => `
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
-    <link rel="icon" href="favicon.png" type="image/png" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/favicon.png" type="image/png" />
     <title>Redux Boilerplate</title>
     ${useCss ? '<link rel="stylesheet" type="text/css" href="/styles.css" />' : ''}
     <script type="text/javascript">
